eee21ac8be2eaf5b872da255b5e0112065418627
Mono <125167202+Mono0087@users.noreply.github.com>
2023-05-06 06:10:27 +0500

    Add numbers input, clear and operations functions

---
+++ main.js
@@ -0,0 +1,78 @@
+const output = document.querySelector('#output');
+const calcBox = document.querySelector('.box');
+const dotBtn = document.querySelector('#dot');
+
+
+let firstNum = 0;
+let secondNum = undefined;
+let currOperation = undefined;
+let result = undefined;
+
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function divide(a, b) {
+    return a / b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+
+function clear() {
+    currentNum = firstNum;
+    firstNum = 0;
+    secondNum = undefined;
+    result = undefined;
+    if (dotBtn.hasAttribute('disabled')) dotBtn.removeAttribute('disabled');
+    output.innerText = 0;
+
+}
+
+function equals(a, b, operation) {
+    switch (operation) {
+        case 'add':
+            return add(a, b);
+            break;
+    }
+}
+
+let currentNum = '';
+function getCurrent(num) {
+    if (num !== '0' && currentNum.charAt(0) === '0') {
+        currentNum = currentNum.substring(1, currentNum.length)
+    } else if (num === '0' && currentNum === '0') {
+        currentNum = '';
+    }
+    if (num === '.') {
+        currentNum += num;
+        document.querySelector('#dot').setAttribute('disabled', 'disabled')
+    } else {
+        currentNum += num;
+    }
+    return currentNum;
+}
+
+function updateOutput(num) {
+    output.innerText = num;
+}
+
+calcBox.addEventListener('click', (e) => {
+    if (currOperation) {// If none operation in time
+
+    } else {// If operation take place
+        if (e.target.classList.contains('number')) {
+            firstNum = +getCurrent(e.target.innerText);
+            updateOutput(currentNum);
+        } else if (e.target.classList.contains('controls')) {
+            if (e.target.id === 'clear') {
+                clear();
+            }
+        }
+    }
+
+    let operObj = { firstNum, secondNum, currOperation, result };
+    console.log(operObj, currentNum)
+})
